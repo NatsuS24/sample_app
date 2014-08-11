8f786940daf152083ad8f9336dab6bf03f5b5e48
Natcha Simsiri <natcha.simsiri@gmail.com>
2014-08-11 16:27:04 +0700

    ch 5 done

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+  	puts("hello #{root_url}")
   end
 
   def help
+  	puts("hello #{help_url}")
   end
 
   def about
+  	puts("hello #{about_url}")
+  end
+
+  def contact 
+  	puts("hello #{contact_url}")
   end
 end
